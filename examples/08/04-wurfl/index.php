646c137bb6135c40ed969900a76e1a57d328bd9a
Lyza Gardner <lyza@cloudfour.com>
2012-05-08 15:06:44 +0200

    Content updates for section 8 (device/feature detection) as well as its examples

--- examples/08/04-wurfl/index.php
+++ examples/08/04-wurfl/index.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <script src="Modernizr-touch.js"></script>
-  <link rel="stylesheet" type="text/stylesheet" href="../../workshop.css" />
+  <link rel="stylesheet" type="text/stylesheet" href="../../fieldnotes.css" />
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   
   <script>
